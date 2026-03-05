135939713a44121f27aed739e59a9a131e18bbf1
murisafonso45 <murisafonso@gmail.com>
2026-03-04 21:54:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,140 @@
-let listaDeCompras = [];
-let rodando = true;
+// var fundo = createSprite(200,200,400,400);
+// fundo.setAnimation("fundo");
+var personagem_1 = createSprite(370,200,20,20);
+var personagem_2_pc = createSprite(35,200,20,20);
+var rede =  createSprite(200,200,2,2);
+var bola = createSprite(200,200,10,10);
 
-while (rodando) {
-    let opcao = prompt(
-        "Lista de compras \n" +
-        "1 Adicionar item\n" +
-        "2 Remover item\n" +
-        "3 Listar itens\n" +
-        "4 Sair\n" +
-        "Escolha uma opção:"
-    );
 
-    switch (opcao) {
-        case "1":
-            let novoItem = prompt("Digite o nome do item:");
-            if (novoItem) {
-                listaDeCompras.push(novoItem); 
-                alert(novoItem + " adicionado!");
-            }
-            break;
 
-        case "2":
-            let itemRemover = prompt("Digite o nome do item a remover:");
-            let index = listaDeCompras.indexOf(itemRemover);
-            if (index !== -1) {
-                listaDeCompras.splice(index, 1); 
-                alert(itemRemover + " removido!");
-            } else {
-                alert("Item não encontrado.");
-            }
-            break;
+bola.setAnimation("bola");
+bola.scale = 0.07;
+personagem_2_pc.scale = 0.5;
+personagem_1.scale = 0.5;
 
-        case "3":
-            if (listaDeCompras.length === 0) {
-                alert("A lista está vazia.");
-            } else {
-                let listaFormatada = "Sua lista:\n";
-                
-                for (let i = 0; i < listaDeCompras.length; i++) {
-                    listaFormatada += (i + 1) + ". " + listaDeCompras[i] + "\n";
-                }
-                alert(listaFormatada);
-            }
-            break;
+var p1 = 0;
+var p2 = 0;
 
-        case "4":
-            rodando = false; 
-            alert("Programa encerrado.");
-            break;
 
-        default:
-            alert("Opção inválida.");
-    }
+personagem_1.setAnimation('img1');
+personagem_2_pc.setAnimation('img2');
+
+
+function draw() {
+   background("yellow");
+   textSize(40) ; 
+
+ textFont('arial');
+ text.shapeColor = 'Red';
+ text(p1, 30,40);
+ text(p2, 360,40);
+ text.de = 1;
+ 
+   
+  if(bola.isTouching(personagem_1) || (bola.isTouching(personagem_2_pc))){
+    
+    playSound('hit.mp3');
+    
+    
+  }
+  
+  
+  
+  if (keyDown('UP_ARROW')){
+    personagem_1.velocityY = -4;
+    
+    
+  }else if (keyDown("down")){
+    
+    personagem_1.velocityY = 4;
+    
+  }
+  
+  if (keyDown("space")){
+    
+    bola.velocityX = 2; 
+    bola.velocityY = 2;
+    
+  }
+  
+  // placar_linha()
+  
+  createEdgeSprites();
+  
+  if(bola.isTouching(topEdge) || (bola.isTouching(bottomEdge))){
+    
+  playSound("sound://category_alerts/vibrant_game_negative_affirmation.mp3");
+    
+  }
+  
+  
+
+  
+
+  
+    if (bola.isTouching(rightEdge) ){
+    
+    p2 = p2 + 1;
+     bola.x =  200;
+    bola.y = 200;
+    
+  }
+  
+  if (bola.isTouching(leftEdge)){
+    
+    p1  =  p1  + 1;
+    bola.x =  200;
+    bola.y = 200;
+    
+  }
+  
+  
+  if (p1 == 3 ){
+    
+  text('PARABÉNS VOCÊ GANHOU O JOGO', 200,110)  ;
+  text('GAME ', 200,200);
+    
+  }
+  
+  if (p2 == 3){
+    
+  text('A MAQUINA GANHOU O JOGO ', 80 ,110)  
+  textFont('arial');
+  text('GAME ', 200,200);
+  p1 = 0;
+  p2 = 0;
+  
+  bola.y  = 0;
+  
+    
+    
+  }
+  
+  
+  personagem_2_pc.y=bola.y;
+  
+  
+  
+  bola.bounceOff(topEdge);
+  bola.bounceOff(bottomEdge);
+  bola.bounceOff(personagem_2_pc);
+  bola.bounceOff(personagem_1);
+  personagem_1.bounceOff(topEdge);
+  personagem_1.bounceOff(bottomEdge);
+  personagem_2_pc.bounceOff(bottomEdge);
+  personagem_2_pc.bounceOff(topEdge);
+  
+  drawSprites();
+  
+ 
 }
+
+
+
+
+
+
+
+
+
+
